6fa23555d883939bf15ad78f79ede160b7d78ff9
danishzulfiqar <danishzulfiqar100@gmail.com>
2024-07-31 17:01:16 +0500

    added get data api root for get 15 min data of ON state for a specific mac address

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
@@ -6,7 +7,15 @@ import numpy as np
 from keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
 import joblib
-from datetime import datetime, date, timedelta
+from datetime import datetime, timedelta, timezone
+import shap
+from pymongo import MongoClient
+from bson import ObjectId
+from reportlab.lib.styles import getSampleStyleSheet
+from pytz import timezone as pytz_timezone
+
+
+# ----------------------------- Model Configs -----------------------------------
 
 app = FastAPI()
 
@@ -19,8 +28,32 @@ scaler = joblib.load('./Models/chenab_scaler.pkl')
 column_names = ['CT1', 'CT2', 'CT3', 'CT_Avg',
                 'total_current', 'therm_temp', 'vibration']
 
-# Define the input data model
 
+# ----------------------------- MongoDB Configuration -----------------------------
+# MongoDB connection
+
+styles = getSampleStyleSheet()
+
+# MongoDB configuration
+MONGO_DB_NAME = "admin"
+MONGO_URL = f"mongodb://wiser:wiser123@115.186.183.129:50001,100.28.143.179:27018,100.28.143.179:27019/admin?replicaSet=wiserReplica"
+
+TIMEZONE_STR = "Asia/Karachi"
+
+chenab_mac_address = "08:D1:F9:A7:4D:FC"
+
+# Connect to MongoDB
+
+
+def connect_mongo():
+    client = MongoClient(MONGO_URL)
+    return client[MONGO_DB_NAME]
+
+# Fetch lab name from MongoDB
+
+# ----------------------------- API Schema -----------------------------
+
+# Define the input data model
 
 class DataRow(BaseModel):
     CT1: float
@@ -38,21 +71,190 @@ class PredictionRequest(BaseModel):
 
 # Define the output data model
 
-
 class PredictionResponse(BaseModel):
     predicted: List[int]
+    fault_cause: List[str]
     faulty_predicted: int
     todays_date: str
     scheduled_maintenance_date: str
     predicted_maintenance_date: str
     degraded_life: float
+    falutToActiveRatio: float
 
 
+# ----------------------------- Helper Functions -----------------------------
+
+# Fetch data for a specified date range from MongoDB
+
+def fetch_data_for_date_range(collection, query):
+    entries_data = collection.find(query)
+    entries_df = pd.DataFrame(list(entries_data))
+    return entries_df
+
+
+def convert_time_string_to_datetime(time_string):
+    if not time_string:
+        return None
+
+    if isinstance(time_string, datetime):
+        return time_string
+
+    date = datetime.today().date()
+
+    try:
+        formatted_time = datetime.strptime(time_string, "%H:%M")
+        combined_datetime = datetime.combine(date, formatted_time.time())
+        result_string = combined_datetime.strftime("%Y-%m-%d %H:%M:%S")
+        return result_string
+    except ValueError as e:
+        print(f"Error converting time string to datetime: {e}")
+        return None
+
+
+def fetch_utilization_data(mac):
+
+    # Connect to MongoDB
+    db = connect_mongo()
+
+    # Calculate the time 15 minutes ago
+    time_threshold = datetime.utcnow() - timedelta(minutes=15)
+
+    # ------- Current Data -----------
+
+    current_query = {
+        "mac": mac,
+        "state": "ON",
+        'created_at': {'$gte': time_threshold},
+    }
+
+    lab_current_collection = db['cts']
+
+    current_data = fetch_data_for_date_range(
+        lab_current_collection, current_query)
+
+    print("Current Data:")
+    print(current_data)
+
+    if current_data.empty:
+        print("No data found")
+        # return msgType:missing and message:No On state data for last 15 minutes
+        return {
+            "msgType": "missing",
+            "message": "No On state CTS data for last 15 minutes"
+        }
+
+    # Using only CT1, CT2, CT3, CT_Avg, total_current, state, created_at
+    if not current_data.empty:
+        current_data = current_data[[
+            "CT1", "CT2", "CT3", "CT_Avg", "total_current", "state", "created_at"]]
+
+    # ---------- Vibration Data ------------
+
+    vibration_query = {
+        "mac": mac,
+        "created_at": {"$gte": time_threshold}
+    }
+
+    lab_vibration_collection = db['vibrations']
+
+    vibration_data = fetch_data_for_date_range(
+        lab_vibration_collection, vibration_query)
+    
+    print(vibration_data)
+
+    if vibration_data.empty:
+        print("No data found")
+        return {
+            "msgType": "missing",
+            "message": "No vibration data for last 15 minutes"
+        }
+
+    # Using only vibration, created_at
+    if not vibration_data.empty:
+        vibration_data = vibration_data[["vibration", "created_at"]]
+
+    # ------------- Temperature Data ---------------
+
+    temperature_query = {
+        "mac": mac,
+        "created_at": {"$gte": time_threshold}
+    }
+
+    lab_temperature_collection = db['thermisters']
+
+    temperature_data = fetch_data_for_date_range(
+        lab_temperature_collection, temperature_query)
+
+    print(temperature_data)
+
+    if temperature_data.empty:
+        print("No data found")
+        return {
+            "msgType": "missing",
+            "message": "No temperature data for last 15 minutes"
+        }  
+
+    # Using only therm_temp, created_at
+    if not temperature_data.empty:
+        temperature_data = temperature_data[["therm_temp", "created_at"]]
+
+    # Joining all dataframes into one based on created_at
+
+    if not current_data.empty and not vibration_data.empty and not temperature_data.empty:
+        data = current_data.merge(vibration_data, on="created_at", how="outer").merge(
+            temperature_data, on="created_at", how="outer")
+    elif not current_data.empty and not vibration_data.empty:
+        data = current_data.merge(vibration_data, on="created_at", how="outer")
+    elif not current_data.empty and not temperature_data.empty:
+        data = current_data.merge(
+            temperature_data, on="created_at", how="outer")
+    elif not vibration_data.empty and not temperature_data.empty:
+        data = vibration_data.merge(
+            temperature_data, on="created_at", how="outer")
+    elif not current_data.empty:
+        data = current_data
+    elif not vibration_data.empty:
+        data = vibration_data
+    elif not temperature_data.empty:
+        data = temperature_data
+    else:
+        data = pd.DataFrame()  # Initialize an empty DataFrame
+        print("No data found")
+
+    data = data.dropna(axis=0)
+
+    # Generating csv file with machine name
+    nodeData = db['nodes'].find_one({"mac": mac}, projection={"_id": 1})
+    node_id = nodeData.get("_id")
+    machine_data = db['machines'].find_one(
+        {"nodeId": ObjectId(node_id)}, projection={"machineName": 1, "_id": 0})
+    machine_name = machine_data.get("machineName")
+
+    file_name = f"{machine_name}.csv"
+    data.to_csv(file_name, index=False)
+
+    # Convert data to json format
+    data = data.to_dict(orient='records')
+
+    
+    return {
+        "msgType": "success",
+        "message": "Data saved successfully",
+        "data": data,
+        "file_name": file_name
+    }
+
+
+# ----------------------------- API Endpoints -----------------------------
+
+
+# Home
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Machine Failure Prediction API"}
 
 
+# predict point for sending input json data and get the prediction
 @app.post("/predict", response_model=PredictionResponse)
 def predict(request: PredictionRequest):
     try:
@@ -62,11 +264,32 @@ def predict(request: PredictionRequest):
 
         # Make predictions
         pred = model.predict(input_data)
-        pred = [1 if y >= 0.5 else 0 for y in pred]
+        pred = [1 if y >= 0.282485 else 0 for y in pred]
 
         predicted = pred
 
-        # Number of faulty ie 1's predicted by the model
+        # Load explainer
+        # explainer = joblib.load('./Models/Drill-14223-chenab_explainer.pkl')
+
+        # Shap explainer
+        explainer = shap.Explainer(model, input_data)
+        shap_values = explainer(input_data)
+
+        # Find the feature that caused each anomaly
+        if isinstance(shap_values, list):
+            # If shap_values is a list (e.g., for binary classification), use the second element
+            shap_df = pd.DataFrame(
+                np.abs(shap_values[1]), columns=column_names)
+        else:
+            # If shap_values is already a 2D array
+            shap_df = pd.DataFrame(
+                np.abs(shap_values.values), columns=column_names)
+
+        # Add feature_cause column to the dataframe
+        shap_df['feature_cause'] = shap_df.idxmax(axis=1)
+        fault_cause = shap_df['feature_cause'].tolist()
+
+        # Number of faulty predictions
         faulty_predicted = pred.count(1)
 
         # Get today's date
@@ -88,20 +311,40 @@ def predict(request: PredictionRequest):
         # Calculate degraded life of the machine
         degraded_life = (24 - n) / 2400
 
+        # Calculate fault to active ratio ie number of 1's / number of input
+        falutToActiveRatio = faulty_predicted / len(pred)
+
         return PredictionResponse(
             predicted=predicted,
+            fault_cause=fault_cause,
             faulty_predicted=faulty_predicted,
             todays_date=todays_date,
             scheduled_maintenance_date=request.maintenance_date,
             predicted_maintenance_date=predicted_maintenance_date_str,
-            degraded_life=degraded_life
+            degraded_life=degraded_life,
+            falutToActiveRatio=falutToActiveRatio
         )
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
-    
 
 
-    
+# get last 15 minutes data to csv file
+@app.post("/{macAddress}/get_interval_data")
+def get_interval_data(macAddress: str):
+    try:
+        returnData = fetch_utilization_data(macAddress)
+        if returnData.get("msgType") == "missing":
+            return {"message": returnData.get("message")}
+        return {"message": f"Data: {returnData}"}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+
+
+
+
+
 
 # command
 
@@ -111,4 +354,3 @@ def predict(request: PredictionRequest):
 
 # docker build -t cenab-model .
 # docker run -d --name chenab-model -p 8000:8000 chenab-model
-
